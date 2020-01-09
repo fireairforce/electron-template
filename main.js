6e3dddb95cd37f0c372ae12357caee6ba29eb944
fireairforce <1344492820@qq.com>
2020-01-09 23:35:44 +0800

    feat:新增devtron调式工具

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 const { app, BrowserWindow } = require('electron');
 
+app.on('ready', () => {
+  require('devtron').install()
+  let mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.loadFile('index.html');
+  // let secondWindow = new BrowserWindow({
+  //   width: 400,
+  //   height: 300,
+  //   webPreferences: {
+  //     nodeIntegration: true
+  //   },
+  //   parent: mainWindow
+  // })
+  // secondWindow.loadFile('second.html')
+  mainWindow.webContents.openDevTools();
+})
